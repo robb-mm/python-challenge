6fda0aa1749f8c570e62218a37faba330dd8ee48
robb-mm <rob_mm@msn.com>
2024-07-16 21:57:07 -0400

    PyBank updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,12 @@
 import os
 import csv
 
-TotalMonths = 0; NetProfit = 0; MaxProfit = 0
+TotalMonths = 0; NetProfit = 0; 
+MinProfit = 0; MaxProfit = 0
 PrevProfit = 0; ProfitChange = 0; AveChange = 0
 ProfitChanges = []
 
-csvpath = os.path.join('Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data_temp.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile)
 
@@ -26,11 +27,16 @@ with open(csvpath) as csvfile:
             MaxProfit_Date = row[0]
             MaxProfit = ProfitChange
             # print(f"{TotalMonths}: {MaxProfit}")
+
+        if (MinProfit > ProfitChange):
+            MinProfit_Date = row[0]
+            MinProfit = ProfitChange
+        
     
     print("Financial Analysis")
     print("------------------")
     print(f"Total Months: {TotalMonths}")
-    print(f"Net Profit: {NetProfit}")
+    print(f"Net Profit: ${NetProfit}")
     print(f"ProfitChanges {ProfitChanges}")
 
     # for x in ProfitChanges:
@@ -43,6 +49,8 @@ with open(csvpath) as csvfile:
     #     # next(x)
 
     AveChange = sum(ProfitChanges) / len(ProfitChanges)
+    print(f"AveChange = {sum(ProfitChanges)} / {len(ProfitChanges)}")
     print(f"Average Change: {AveChange}")
 
-    print(f"Greatest Increase in Profits: {MaxProfit_Date} $({MaxProfit})")
+    print(f"Greatest Increase in Profits: {MaxProfit_Date} (${MaxProfit})")
+    print(f"Greatest Decrease in Profits: {MinProfit_Date} (${MinProfit})")
